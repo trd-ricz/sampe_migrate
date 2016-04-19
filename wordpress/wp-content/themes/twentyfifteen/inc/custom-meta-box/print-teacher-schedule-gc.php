926bcdc6d2736a55075a0eb09c8d37c6c4f5e3bc
Brylle John Empenida <trd.bryllejohnempenida@gmail.com>
2016-04-19 19:10:31 +0800

    add yellow bg if new student and white for old student

--- wordpress/wp-content/themes/twentyfifteen/inc/custom-meta-box/print-teacher-schedule-gc.php
+++ wordpress/wp-content/themes/twentyfifteen/inc/custom-meta-box/print-teacher-schedule-gc.php
@@ -36,16 +36,26 @@
 	$studentsgc8 = array();
 	$studentsgc9 = array();
 	$studentsgc10 = array();
-	$colorgc2 = array();
-	$colorgc3 = array();
-	$colorgc4 = array();
-	$colorgc5 = array();
-	$colorgc6 = array();
-	$colorgc7 = array();
-	$colorgc8 = array();
-	$colorgc9 = array();
-	$colorgc10 = array();
+
+	$studgcstatus2 = array();
+	$studgcstatus3 = array();
+	$studgcstatus4 = array();
+	$studgcstatus5 = array();
+	$studgcstatus6 = array();
+	$studgcstatus7 = array();
+	$studgcstatus8 = array();
+	$studgcstatus9 = array();
+	$studgcstatus10 = array();
+
 	$ssdate2 = array();
+	$ssdate3 = array();
+	$ssdate4 = array();
+	$ssdate5 = array();
+	$ssdate6 = array();
+	$ssdate7 = array();
+	$ssdate8 = array();
+	$ssdate9 = array();
+	$ssdate10 = array();
 
 	foreach($all_class_type as $single_class) {
 		foreach($new_data_print_teacher as $tsched_gc) {
@@ -53,7 +63,8 @@
 				$studentsgc2[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time2"][0];
 				$gcroom = $tsched_gc["sched"]["time2"][2];
-				$colorgc2[] = $tsched_gc['status'];
+
+				$studgcstatus2[] = $tsched_gc['status'];
 				$ssdate2[] = $tsched_gc['start_date'];
 			}
 
@@ -61,54 +72,76 @@
 				$studentsgc3[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time3"][0];
 				$gcroom = $tsched_gc["sched"]["time3"][2];
+
+				$studgcstatus3[] = $tsched_gc['status'];
+				$ssdate3[] = $tsched_gc['start_date'];
 			}
 
 			if ($tsched_gc["sched"]["time4"][1] == $single_class->post_title) {
 				$studentsgc4[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time4"][0];
 				$gcroom = $tsched_gc["sched"]["time4"][2];
+
+				$studgcstatus4[] = $tsched_gc['status'];
+				$ssdate4[] = $tsched_gc['start_date'];
 			}
 
 			if ($tsched_gc["sched"]["time5"][1] == $single_class->post_title) {
 				$studentsgc5[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time5"][0];
 				$gcroom = $tsched_gc["sched"]["time5"][2];
+
+				$studgcstatus5[] = $tsched_gc['status'];
+				$ssdate5[] = $tsched_gc['start_date'];
 			}
 
 			if ($tsched_gc["sched"]["time6"][1] == $single_class->post_title) {
 				$studentsgc6[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time6"][0];
 				$gcroom = $tsched_gc["sched"]["time6"][2];
+
+				$studgcstatus6[] = $tsched_gc['status'];
+				$ssdate6[] = $tsched_gc['start_date'];
 			}
 
 			if ($tsched_gc["sched"]["time7"][1] == $single_class->post_title) {
 				$studentsgc7[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time7"][0];
 				$gcroom = $tsched_gc["sched"]["time7"][2];
+
+				$studgcstatus7[] = $tsched_gc['status'];
+				$ssdate7[] = $tsched_gc['start_date'];
 			}
 
 			if ($tsched_gc["sched"]["time8"][1] == $single_class->post_title) {
 				$studentsgc8[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time8"][0];
 				$gcroom = $tsched_gc["sched"]["time8"][2];
+
+				$studgcstatus8[] = $tsched_gc['status'];
+				$ssdate8[] = $tsched_gc['start_date'];
 			}
 
 			if ($tsched_gc["sched"]["time9"][1] == $single_class->post_title) {
 				$studentsgc9[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time9"][0];
 				$gcroom = $tsched_gc["sched"]["time9"][2];
+
+				$studgcstatus9[] = $tsched_gc['status'];
+				$ssdate9[] = $tsched_gc['start_date'];
 			}
 
 			if ($tsched_gc["sched"]["time10"][1] == $single_class->post_title) {
 				$studentsgc10[] = $tsched_gc["name"];
 				$gcteacher = $tsched_gc["sched"]["time10"][0];
 				$gcroom = $tsched_gc["sched"]["time10"][2];
+
+				$studgcstatus10[] = $tsched_gc['status'];
+				$ssdate10[] = $tsched_gc['start_date'];
 			}
 		} // end 2nd foreach
 
-
 		?>
-
 
 		<tr class="tym-parent">
 			<td>
@@ -123,39 +156,151 @@
 			<td colspan="2">
 
 				<?php
-				$sstoday = date('Y-m-d');
+				$sstodaygc = date('Y-m-d');
 				foreach ($studentsgc2 as $key => $value) {
-					$val2 = $colorgc2[$key];
-					$val3 = $ssdate2[$key];
+					$sgcstatus = $studgcstatus2[$key];
+					$ssd2gc = $ssdate2[$key];
 
-					$val4 = date('Y-m-d', strtotime($val3 . ' + 7 days'));
+					$newssd2gc = date('Y-m-d', strtotime($ssd2gc . ' + 7 days'));
 
-					echo $value.$val2.$val4."; ";
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd2gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				}
 				?></td>
 			<td colspan="2"><?php foreach ($studentsgc3 as $key => $value) {
-					echo $value."; ";
+					$sgcstatus = $studgcstatus3[$key];
+					$ssd3gc = $ssdate3[$key];
+
+					$newssd3gc = date('Y-m-d', strtotime($ssd3gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd3gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 			<td colspan="2"><?php foreach ($studentsgc4 as $key => $value) {
-					echo $value."; ";
+					$sgcstatus = $studgcstatus4[$key];
+					$ssd4gc = $ssdate4[$key];
+
+					$newssd4gc = date('Y-m-d', strtotime($ssd4gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd4gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 			<td colspan="2"><?php foreach ($studentsgc5 as $key => $value) {
-					echo $value."; ";
+					$sgcstatus = $studgcstatus5[$key];
+					$ssd5gc = $ssdate5[$key];
+
+					$newssd5gc = date('Y-m-d', strtotime($ssd5gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd5gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 			<td colspan="2"><?php foreach ($studentsgc6 as $key => $value) {
-					echo $value."; ";
+					$sgcstatus = $studgcstatus6[$key];
+					$ssd6gc = $ssdate6[$key];
+
+					$newssd6gc = date('Y-m-d', strtotime($ssd6gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd6gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 			<td colspan="2"><?php foreach ($studentsgc7 as $key => $value) {
-					echo $value."; ";
+					$sgcstatus = $studgcstatus7[$key];
+					$ssd7gc = $ssdate7[$key];
+
+					$newssd7gc = date('Y-m-d', strtotime($ssd7gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd7gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 			<td colspan="2"><?php foreach ($studentsgc8 as $key => $value) {
-					echo $value."; ";
+					$sgcstatus = $studgcstatus8[$key];
+					$ssd8gc = $ssdate8[$key];
+
+					$newssd8gc = date('Y-m-d', strtotime($ssd8gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd8gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 			<td colspan="2"><?php foreach ($studentsgc9 as $key => $value) {
-					echo $value."; ";
+						$sgcstatus = $studgcstatus9[$key];
+					$ssd9gc = $ssdate9[$key];
+
+					$newssd9gc = date('Y-m-d', strtotime($ssd9gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd9gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+						echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 			<td colspan="2"><?php foreach ($studentsgc10 as $key => $value) {
-					echo $value."; ";
+						$sgcstatus = $studgcstatus10[$key];
+					$ssd10gc = $ssdate10[$key];
+
+					$newssd10gc = date('Y-m-d', strtotime($ssd10gc . ' + 7 days'));
+
+					if ($sgcstatus == 'NEW STUDENT' && $sstodaygc >= $newssd10gc) {
+						$colorgc = 'white';
+					} elseif ($sgcstatus == 'OLD STUDENT') {
+						$colorgc = 'white';
+					} else {
+						$colorgc = 'yellow';
+					}
+
+					echo '<span class="'.$colorgc.'">'.$value.";</span> ";
 				} ?></td>
 		</tr>
 		<?php
@@ -171,7 +316,6 @@
 		$studentsgc8 = array();
 		$studentsgc9 = array();
 		$studentsgc10 = array();
-		$colorgc2 = array();
 
 	} // end all class
 	?>
--- wordpress/wp-content/themes/twentyfifteen/inc/custom-meta-box/print-teacher-schedule-mm.php
+++ wordpress/wp-content/themes/twentyfifteen/inc/custom-meta-box/print-teacher-schedule-mm.php
@@ -26,6 +26,8 @@
 	</thead>
 	<tbody>
 	<?php
+	error_reporting(0);
+
 	$stud_name = array();
 	$stud_room = array();
 	$clstype = array();
@@ -34,7 +36,7 @@
 	foreach($all_teachers_to_display as $teacher_single) {
 		foreach($new_data_print_teacher as $tsched) {
 			if ($tsched["sched"]["time2"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time2"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time2"][1], " MM") !== false) {
 					$clstype[2] = $tsched["sched"]["time2"][1];
 					$stud_name[2] = $tsched['name'];
 					$stud_room[2] = $tsched["sched"]["time2"][2];
@@ -55,7 +57,7 @@
 			}
 
 			if ($tsched["sched"]["time3"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time3"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time3"][1], " MM") !== false) {
 					$clstype[3] = $tsched["sched"]["time3"][1];
 					$stud_name[3] = $tsched['name'];
 					$stud_room[3] = $tsched["sched"]["time3"][2];
@@ -76,7 +78,7 @@
 			}
 
 			if ($tsched["sched"]["time4"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time4"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time4"][1], " MM") !== false) {
 					$clstype[4] = $tsched["sched"]["time4"][1];
 					$stud_name[4] = $tsched['name'];
 					$stud_room[4] = $tsched["sched"]["time4"][2];
@@ -97,7 +99,7 @@
 			}
 
 			if ($tsched["sched"]["time5"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time5"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time5"][1], " MM") !== false) {
 					$clstype[5] = $tsched["sched"]["time5"][1];
 					$stud_name[5] = $tsched['name'];
 					$stud_room[5] = $tsched["sched"]["time5"][2];
@@ -118,7 +120,7 @@
 			}
 
 			if ($tsched["sched"]["time6"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time6"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time6"][1], " MM") !== false) {
 					$clstype[6] = $tsched["sched"]["time6"][1];
 					$stud_name[6] = $tsched['name'];
 					$stud_room[6] = $tsched["sched"]["time6"][2];
@@ -139,7 +141,7 @@
 			}
 
 			if ($tsched["sched"]["time7"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time7"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time7"][1], " MM") !== false) {
 					$clstype[7] = $tsched["sched"]["time7"][1];
 					$stud_name[7] = $tsched['name'];
 					$stud_room[7] = $tsched["sched"]["time7"][2];
@@ -160,7 +162,7 @@
 			}
 
 			if ($tsched["sched"]["time8"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time8"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time8"][1], " MM") !== false) {
 					$clstype[8] = $tsched["sched"]["time8"][1];
 					$stud_name[8] = $tsched['name'];
 					$stud_room[8] = $tsched["sched"]["time8"][2];
@@ -181,7 +183,7 @@
 			}
 
 			if ($tsched["sched"]["time9"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time9"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time9"][1], " MM") !== false) {
 					$clstype[9] = $tsched["sched"]["time9"][1];
 					$stud_name[9] = $tsched['name'];
 					$stud_room[9] = $tsched["sched"]["time9"][2];
@@ -202,7 +204,7 @@
 			}
 
 			if ($tsched["sched"]["time10"][0] == strtoupper($teacher_single->display_name)) {
-				if (strpos($tsched["sched"]["time10"][1], "MM") !== false) {
+				if (strpos($tsched["sched"]["time10"][1], " MM") !== false) {
 					$clstype[10] = $tsched["sched"]["time10"][1];
 					$stud_name[10] = $tsched['name'];
 					$stud_room[10] = $tsched["sched"]["time10"][2];
